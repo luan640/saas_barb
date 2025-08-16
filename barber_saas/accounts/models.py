b174b838c2fa1ad426fd29826d01e88d0f862ca9
luan <luan640@gmail.com>
2025-08-15 21:11:38 -0300

    feat: custom user por e-mail e fluxo de serviços

--- barber_saas/accounts/models.py
+++ barber_saas/accounts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from .managers import UserManager
 
 class User(AbstractUser):
     username = None
@@ -8,6 +8,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()   # <<< importante!
 
     def __str__(self):
         return self.email
